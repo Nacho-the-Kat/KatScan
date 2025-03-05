c3c9cb9157b78771d0efa236a3c989a14100f09e
argonmining <ashton@onargon.com>
2025-03-05 11:36:22 -0800

    fresh start

--- craco.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-const path = require('path');
-module.exports = {
-    webpack: {
-        alias: {
-            'styles': path.resolve(__dirname, './src/styles'),
-        }
-    }
-};
--- src/interfaces/MintData.ts
+++
@@ -1,4 +0,0 @@
-export type MintData = {
-    tick: string
-    mintTotal: number
-}
